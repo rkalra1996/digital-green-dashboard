78f2f62dab804829eee0826277a654ec23b4655b
rishabh kalra <rishabh.kalra@niit.com>
2020-03-23 11:48:43 +0530

    added shared header

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardCoreComponent } from './components/dashboard-core/dashboard-core.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [DashboardCoreComponent],
   imports: [
     CommonModule,
+    SharedModule,
     DashboardRoutingModule
   ]
 })
