68ae52e26efc731658333ca98dd3dbf369edbe90
rishabh kalra <rishabh.kalra@niit.com>
2020-03-26 12:53:11 +0530

    updated local config

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseEndPoint: 'https://db0ce24a.ngrok.io',
+  baseEndPoint: 'http://localhost:3000',
   authToken: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJpc2hhYmhrYWxyYTk2IiwiZW1haWwiOiJyaXNoYWJoa2FscmE5NkBnbWFpbC5jb20iLCJpYXQiOjE1ODA5ODg5Njd9.H6xfNNNe2kUVBF-FBG8ChB1b_j2tMBnmJQOsafPxwNo',
   urls: {
     getReportEndPoint: '/dashboard/get-report'
