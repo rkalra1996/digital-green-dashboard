a26955b3ca4116631a929951558e9d26321d1585
rishabh kalra <rishabh.kalra@niit.com>
2020-03-31 14:08:30 +0530

    updated environment variable for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseEndPoint: 'http://52.221.207.221:3002',
+  baseEndPoint: 'http://socion-pda-dashboard.stackroute.com:3015',
   authToken: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InJpc2hhYmhrYWxyYTk2IiwiZW1haWwiOiJyaXNoYWJoa2FscmE5NkBnbWFpbC5jb20iLCJpYXQiOjE1ODA5ODg5Njd9.H6xfNNNe2kUVBF-FBG8ChB1b_j2tMBnmJQOsafPxwNo',
   urls: {
     getReportEndPoint: '/dashboard/get-report'
